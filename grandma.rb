4743f3341ce520d44b0c67b553883cba67d27ed8
Sung Bae <singsang2@gmail.com>
2018-10-06 03:08:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,3 +14,10 @@
 
 def speak_to_grandma(phrase)
   if !(phrase == phrase.upcase)
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
+  END
+END
